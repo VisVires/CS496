10ffca4084a9a6112a62347ba4ea7718e1849fee
georgew <wgeorge@outlook.com>
2017-05-09 22:20:29 -0700

    added close buttons to all views

--- Assignment5/AndroidUI/app/src/main/java/com/androidui/georgew/androidui/linear_layout_vertical.java
+++ Assignment5/AndroidUI/app/src/main/java/com/androidui/georgew/androidui/linear_layout_vertical.java
@@ -2,6 +2,8 @@ package com.androidui.georgew.androidui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class linear_layout_vertical extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class linear_layout_vertical extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linear_layout_vertical);
+
+        Button btnClose = (Button) findViewById(R.id.btnClose);
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                //Closing SecondScreen Activity
+                finish();
+            }
+        });
     }
 }
--- Assignment5/AndroidUI/app/src/main/java/com/androidui/georgew/androidui/relative_layout.java
+++ Assignment5/AndroidUI/app/src/main/java/com/androidui/georgew/androidui/relative_layout.java
@@ -2,6 +2,8 @@ package com.androidui.georgew.androidui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class relative_layout extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class relative_layout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relative_layout);
+
+        Button btnClose = (Button) findViewById(R.id.btnClose);
+
+        btnClose.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                //Closing SecondScreen Activity
+                finish();
+            }
+        });
     }
 }
