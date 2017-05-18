9c1190ac74a14c4e3598a55ccbba97f7bc83f40e
georgew <wgeorge@outlook.com>
2017-05-17 19:37:53 -0700

    added location permissions and some functionality

--- Assignment6/SQLiteGPS/app/src/main/java/com/androidui/georgew/sqlitegps/MainActivity.java
+++ Assignment6/SQLiteGPS/app/src/main/java/com/androidui/georgew/sqlitegps/MainActivity.java
@@ -1,13 +1,85 @@
 package com.androidui.georgew.sqlitegps;
 
+import android.*;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+
+
+    private GoogleApiClient googleApiClient;
+    private Location myLocation;
+    private LocationListener locationListener;
+    private LocationRequest locationRequest;
+    private TextView latitude;
+    private TextView longitude;
+    private static final int LOCATION_PERMISSION_RESULT = 17;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (googleApiClient == null) {
+            googleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+        latitude = (TextView) findViewById(R.id.lat_coordinate);
+        longitude = (TextView) findViewById(R.id.long_coordinate);
+        latitude.setText("Activity Created");
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(5000);
+        locationListener = (LocationListener) (location) {
+            if (location != null) {
+                latitude.setText(String.valueOf(location.getLatitude()));
+                longitude.setText(String.valueOf(location.getLongitude()));
+            } else {
+                latitude.setText("None");
+            }
+        };
+    }
+    
+    protected void onStop(){
+        googleApiClient.disconnect();
+        super.onStop();
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        latitude.setText("onConnect");
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            ActivityCompat.requestPermissions(this, new String[](Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, LOCATION_PERMISSION_RESULT););
+        longitude.setText("NO PERMISSIONS");
+        return;
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 }
