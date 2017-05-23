7df576facb24983ee88b670dc070afb43728c881
georgew <wgeorge@outlook.com>
2017-05-23 12:32:45 -0700

    finished skeleton of oauth request

--- Assignment7/app/src/main/java/com/oauth/georgew/oauthapp/MainActivity.java
+++ Assignment7/app/src/main/java/com/oauth/georgew/oauthapp/MainActivity.java
@@ -1,21 +1,79 @@
 package com.oauth.georgew.oauthapp;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import net.openid.appauth.AuthState;
+import net.openid.appauth.AuthorizationException;
+import net.openid.appauth.AuthorizationRequest;
+import net.openid.appauth.AuthorizationService;
+import net.openid.appauth.AuthorizationServiceConfiguration;
+import net.openid.appauth.ResponseTypeValues;
+
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
+    String CLIENT_ID = "327991664103-3ij54mg8q56qeclp6a1i4jjn7jtfk8uj.apps.googleusercontent.com";
+    private AuthorizationService completeAuthorizationService;
+    private AuthState authState;
+    TextView access_token;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        String CLIENT_ID = "327991664103-3ij54mg8q56qeclp6a1i4jjn7jtfk8uj.apps.googleusercontent.com";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*authState.performActionWithFreshTokens(completeAuthorizationService, new AuthState.AuthStateAction() {
+            @Override
+            public void execute(@Nullable String accessToken, @Nullable String idToken, @Nullable AuthorizationException e) {
+                access_token.setText(accessToken);
+            }
+        });*/
+    }
+
+    @Override
+    protected void onStart(){
+        authState = getOrCreateAuthState();
+        super.onStart();
+    }
+
+    AuthState getOrCreateAuthState(){
+        AuthState authorization = null;
+        SharedPreferences authorizationPreference = getSharedPreferences("auth", MODE_PRIVATE);
+        String stateJson = authorizationPreference.getString("stateJson", null);
+        if(stateJson != null){
+            try {
+                authorization = AuthState.jsonDeserialize(stateJson);
+            } catch (JSONException je){
+                je.printStackTrace();
+                return null;
+            }
+        }
+        if (authorization != null && authorization.getAccessToken() != null){
+            return authorization;
+        } else {
+            createAuthState();
+            return null;
+        }
     }
 
     void createAuthState(){
         Uri authEndPoint = new Uri.Builder().scheme("https").authority("accounts.google.com").path("o/oauth2/v2/auth").build();
         Uri tokenEndPoint = new Uri.Builder().scheme("https").authority("www.googleapis.com").path("/oauth2/v4/token").build();
-        Uri redirect = new Uri.Builder().scheme("com.oauth.georgew.oauthapp").path("foo").build();
+        Uri redirect = new Uri.Builder().scheme("com.oauth.georgew.oauthapp").path("path").build();
+
+        AuthorizationServiceConfiguration config = new AuthorizationServiceConfiguration(authEndPoint, tokenEndPoint, null);
+        AuthorizationRequest req = new AuthorizationRequest.Builder(config, CLIENT_ID, ResponseTypeValues.CODE, redirect).setScopes(
+                "https://www.googleapis.com/auth/plus.me", "https://www.googleapis.com/auth/plus.stream.write", "https://www.googleapis.com/auth/plus.stream.read"
+        ).build();
+
+        Intent authComplete = new Intent(this, AuthComplete.class);
+        completeAuthorizationService.performAuthorizationRequest(req, PendingIntent.getActivity(this, req.hashCode(), authComplete, 0));
     }
 }
