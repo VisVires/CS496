2db76c1098245d32b8fefbd60080f25dfebd838c
georgew <wgeorge@outlook.com>
2017-05-23 09:52:02 -0700

    added credentials

--- Assignment7/app/src/main/java/com/oauth/georgew/oauthapp/MainActivity.java
+++ Assignment7/app/src/main/java/com/oauth/georgew/oauthapp/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String CLIENT_ID = "327991664103-3ij54mg8q56qeclp6a1i4jjn7jtfk8uj.apps.googleusercontent.com";
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
