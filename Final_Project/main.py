3df35d099fb60e0e138c55882ac5f43bc5c2c8ac
georgew <wgeorge@outlook.com>
2017-06-03 14:46:05 -0700

    got datastore and structure locked down on backend

--- Final_Project/main.py
+++ Final_Project/main.py
@@ -3,17 +3,10 @@ from datetime import datetime
 import webapp2
 import json
 
-class User:
-	id = ndb.StringProperty(required=True)
-	first_name = ndb.StringProperty()
-	last_name = ndb.StringProperty()
-	email = ndb.StringProperty()
-	male = ndb.BooleanProperty()
 
-class Measurements:
-	measurement_date = ndb.StringProperty(required=True)
-	height = ndb.IntegerProperty()
-	weight = ndb.FloatProperty()
+class Measurement(ndb.Model):
+	id = ndb.StringProperty()
+	measurement_date = ndb.StringProperty()
 	neck_circ = ndb.FloatProperty()
 	chest_circ = ndb.FloatProperty()
 	upper_arm_circ = ndb.FloatProperty()
@@ -22,33 +15,117 @@ class Measurements:
 	hip_circ = ndb.FloatProperty()
 	thigh_circ = ndb.FloatProperty()
 	calf_circ = ndb.FloatProperty()
+
+class Pinches(ndb.Model):
+	id = ndb.StringProperty() 
+	weight = ndb.FloatProperty()
 	body_fat_measure = ndb.FloatProperty()
 	body_density_measure = ndb.FloatProperty()
-
-class Pinches:
-	pinch_test_date = ndb.StringProperty(required=True)
-	number = ndb.IntegerProperty(required=True)
+	pinch_test_date = ndb.StringProperty()
+	number = ndb.IntegerProperty()
 	chest_pinch = ndb.FloatProperty()
 	bicep_pinch = ndb.FloatProperty()
 	tricep_pinch = ndb.FloatProperty()
 	abdominal_pinch = ndb.FloatProperty()
 	thigh_pinch = ndb.FloatProperty()
 	suprailiac_pinch = ndb.FloatProperty()
-	supraclavicular_pinch = ndb.FloatProperty()
+	subscapular_pinch = ndb.FloatProperty()
 	midaxillary_pinch = ndb.FloatProperty()
 	lower_back_pinch = ndb.FloatProperty()
 	supraspinale_pinch = ndb.FloatProperty()
 
+class User(ndb.Model):
+	id = ndb.StringProperty()
+	first_name = ndb.StringProperty()
+	last_name = ndb.StringProperty()
+	email = ndb.StringProperty()
+	male = ndb.BooleanProperty()
+	age = ndb.IntegerProperty()
+	height = ndb.IntegerProperty()
+	measurements = ndb.StructuredProperty(Measurement, repeated=True)
+	pinches = ndb.StructuredProperty(Pinches, repeated=True)
 
 class Measurement_Handler(webapp2.RequestHandler):
+	#add new set of measurments
+	def post(self):
+		measurement_data = json.loads(self.request.body)
+		if measurement_data.get('user'):
+			user = ndb.Key(urlsafe=measurement_data.get('user')).get()
+			current_date = datetime.now().date().strftime('%m/%d/%Y')
+			new_measurement = Measurement(measurement_date=current_date)
+			if measurement_data.get('neck_circ'):
+				new_measurement.neck_circ = measurement_data['neck_circ']
+			if measurement_data.get('chest_circ'):
+				new_measurement.chest_circ = measurement_data['chest_circ']
+			if measurement_data.get('upper_arm_circ'):
+				new_measurement.upper_arm_circ = measurement_data['upper_arm_circ']
+			if measurement_data.get('fore_arm_circ'):
+				new_measurement.fore_arm_circ = measurement_data['fore_arm_circ']
+			if measurement_data.get('waist_circ'):
+				new_measurement.waist_circ = measurement_data['waist_circ']
+			if measurement_data.get('hip_circ'):
+				new_measurement.hip_circ = measurement_data['hip_circ']
+			if measurement_data.get('thigh_circ'):
+				new_measurement.thigh_circ = measurement_data['thigh_circ']
+			if measurement_data.get('calf_circ'):
+				new_measurement.calf_circ = measurement_data['calf_circ']
+			new_measurement.put()
+			new_measurement.id = new_measurement.key.urlsafe()
+			new_measurement.put()
+			user.measurements.append(new_measurement)
+			user.put()
+			user_dict = user.to_dict()
+			self.response.write(json.dumps(user_dict))
+		
+			
+	
+	def get(self, id=None):
+		if id:
+			curr_measurement = ndb.Key(urlsafe=id).get()
+			curr_measurement_dict = curr_measurement.to_dict()
+			self.response.write(json.dumps(curr_measurement_dict))
+
 
 class PinchTest_Handler(webapp2.RequestHandler):
+	#add new set of pinches for 4 pinch test
+	def post(self):
+		pinch_data = json.loads(self.request.body)
+		if pinch_data.get('user'):
+			curr_user = ndb.Key(urlsafe=pinch_data.get('user')).get()
+			current_date = datetime.now().date().strftime('%m/%d/%Y')
+			new_pinches = Pinches(pinch_test_date=current_date)
+			new_pinches.bicep_pinch = pinch_data['bicep']
+			new_pinches.tricep_pinch = pinch_data['tricep']
+			new_pinches.subscapular_pinch = pinch_data['subscapular']
+			new_pinches.suprailiac_pinch = pinch_data['suprailiac']
+			new_pinches.put()
+			new_pinches.id = new_pinches.key.urlsafe()
+			new_pinches.put()
+			curr_user.pinches.append(new_pinches)
+			curr_user.put()
+			curr_user_dict = curr_user.to_dict()
+			self.response.write(json.dumps(curr_user_dict))
 
+	def get(self, id=None):
+		if id:
+			curr_pinches = ndb.Key(urlsafe=id).get()
+			curr_pinches_dict = curr_pinches.to_dict()
+			self.response.write(json.dumps(curr_pinches_dict))
 
 #test class
 class MainPage(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello, this is just a test page3')
+	def post(self):
+		user_data = json.loads(self.request.body)
+		new_user = User()
+		new_user.first_name = user_data['first_name']
+		new_user.last_name = user_data['last_name']
+		new_user.email = user_data['email']
+		new_user.put()
+		new_user.id = new_user.key.urlsafe()
+		new_user.put()
+		new_user_dict = new_user.to_dict()
+		self.response.write(json.dumps(new_user_dict))
+
 
 #create patch method
 allowed_methods = webapp2.WSGIApplication.allowed_methods
@@ -59,5 +136,7 @@ webapp2.WSGIApplication.allowed_methods = new_allowed_methods
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/measurements', Measurement_Handler),
-    ('/pinchtest', PinchTest_Handler)
+    ('/pinchtest', PinchTest_Handler),
+    ('/measurements/(.*)', Measurement_Handler),
+    ('/pinchtest/(.*)', PinchTest_Handler)
 ], debug=True)
